58bedb7051ea6f82eceffb5c36db85001d28db73
Maria <achigarpia@gmail>
2022-12-01 15:10:12 -0300

    lancha hardcodeada

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-document.querySelector("#menos").addEventListener("click", (e)=>{
-    animalito.classList.toggle("animalito-rotate-menos")
+// document.querySelector("#menos").addEventListener("click", (e)=>{
+//     animalito.classList.toggle("animalito-rotate-menos")
+// })
+// document.querySelector("#mas").addEventListener("click", (e)=>{
+//     animalito.classList.toggle("animalito-rotate-mas")
+// })
+// document.querySelector(".boton").addEventListener("click", (e)=>{
+//     animalito.classList.remove("animalito-rotate-mas")
+//     animalito.classList.remove("animalito-rotate-menos")
+// })
+//cuando toque los btn, haga un cambio de img con un setTimeout
+document.querySelector("#apuntar0").addEventListener("click", (e)=>{
+    console.log("hola 0grados")
+    animalito.classList.toggle("css-apuntar0")
 })
-document.querySelector("#mas").addEventListener("click", (e)=>{
-    animalito.classList.toggle("animalito-rotate-mas")
+document.querySelector("#apuntar90").addEventListener("click", (e)=>{
+    animalito.classList.toggle("css-apuntar90")
 })
-document.querySelector(".boton").addEventListener("click", (e)=>{
-    animalito.classList.remove("animalito-rotate-mas")
-    animalito.classList.remove("animalito-rotate-menos")
+document.querySelector("#apuntar180").addEventListener("click", (e)=>{
+    animalito.classList.toggle("css-apuntar180")
+})
+document.querySelector("#apuntar270").addEventListener("click", (e)=>{
+    console.log("hola 0grados")
+    animalito.classList.toggle("css-apuntar270")
 })
